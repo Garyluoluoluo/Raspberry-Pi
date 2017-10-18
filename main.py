cb8d3d0abcad3ddd14197213aafd1f77b7943f91
Garyluoluoluo <506494050@qq.com>
2017-10-18 20:28:19 +0800

    Add files via upload
    support  test step  for each key

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 from time import strftime
 from random import choice
 import csv
+import RPi.GPIO as GPIO    
+
 
 
 # 输入按键个数范围 1到6个按键
@@ -54,18 +56,66 @@ def wirte_log(cmd_list_str):
         write_step.writerows(cmd_list_str)
     print('write Log successfully')
 
+#init GPIO
+def init_GPIO():
+    GPIO.setwarnings(False)
+    # BOARD编号方式，基于插座引脚编号    
+    GPIO.setmode(GPIO.BOARD)    
+    # 输出模式
+    GPIO.setup(3, GPIO.OUT)
+    GPIO.setup(5, GPIO.OUT)
+    GPIO.setup(7, GPIO.OUT)
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
+    GPIO.setup(15, GPIO.OUT)
+    #set to low 
+    GPIO.output(3, GPIO.LOW)
+    GPIO.output(5, GPIO.LOW)
+    GPIO.output(7, GPIO.LOW)
+    GPIO.output(11, GPIO.LOW)
+    GPIO.output(13, GPIO.LOW)
+    GPIO.output(15, GPIO.LOW) 
+def key_mapping(cmd,out_status=''):
+    if out_status is 'GPIO.HIGH'or 'GPIO.LOW':
+        if cmd is 1 :
+            GPIO.output(3,out_status)
+        elif cmd is 2:
+            GPIO.output(5,out_status)
+        elif cmd is 3:
+            GPIO.output(7,out_status)
+        elif cmd is 4:
+            GPIO.output(11,out_status)
+        elif cmd is 5:
+            GPIO.output(13,out_status)
+        elif cmd is 6:
+            GPIO.output(15,out_status)
+        else:
+            print('something wrong')
 #——————————————————下面是主函数————————————————
 key_number = input_key_number()
 key_times = input_key_times()
 key_clicks = input_key_clicks()
+init_GPIO()
 input('if the DUT is fixed ,please press ENTER ')
+ 
+list_count = 0
+
+
+for test_count in range(1,(key_number+1)):
+    input('test the Key'+str(test_count)+'#,please check')
+    key_mapping(test_count,GPIO.HIGH)
+    time.sleep(1)
+    key_mapping(test_count,GPIO.LOW)
+print('AutoMachine will be activated after 10 s,please make sure the DUT is fixed')
+
 for i in range(10):
     x = str(10 - i)
-    print('AutoMachine will be activated after '+ x +'s,please make sure the DUT is fixed')
+    print(x +'s')
     time.sleep(1)
 cmd_list = get_cmdlist(key_number,key_clicks)
 cmd_list_str=[str(i) for i in cmd_list]
 print('cmd is running ,please waiting .............')
+print('Program is running , press ctrl + c to stop')
 
 filename = 'ScriptLog' + strftime("%Y%m%d%H%M")
 with open(filename + '.csv', 'a', newline='') as write_cmd:
@@ -74,17 +124,45 @@ with open(filename + '.csv', 'a', newline='') as write_cmd:
     write_cmd.close()
 
 for cmd in cmd_list:
-    time.sleep(key_times)
+    list_count += 1
     time_current = strftime("%Y%m%d-%H:%M:%S")
-    print('Program is running , press ctrl + c to stop')
-    log = [str(cmd),time_current]
+    if cmd is 1:
+        GPIO.output(3, GPIO.HIGH)
+        time.sleep(key_times)
+        GPIO.output(3, GPIO.LOW)
+    elif cmd is 2:
+        GPIO.output(5, GPIO.HIGH)
+        time.sleep(key_times)
+        GPIO.output(5, GPIO.LOW)
+    elif cmd is 3:
+        GPIO.output(7, GPIO.HIGH)
+        time.sleep(key_times)
+        GPIO.output(7, GPIO.LOW)
+    elif cmd is 4:
+        GPIO.output(11, GPIO.HIGH)
+        time.sleep(key_times)
+        GPIO.output(11, GPIO.LOW)
+    elif cmd is 5:
+        GPIO.output(13, GPIO.HIGH)
+        time.sleep(key_times)
+        GPIO.output(13, GPIO.LOW)
+    elif cmd is 6:
+        GPIO.output(15, GPIO.HIGH)
+        time.sleep(key_times)
+        GPIO.output(15, GPIO.LOW)
+    else:
+        print('something wrong')
+        break
+    time.sleep(0.2)
+    log = [str(cmd),time_current] 
     with open(filename + '.csv', 'a', newline='') as write_cmd:
         write_step = csv.writer(write_cmd)
         write_step = csv.writer(write_cmd)
         write_step.writerow(log)
         write_cmd.close()
 
-
+if list_count == len(cmd_list):
+    print('Program run successfully')
 
 #wirte_log(cmd_list_str)
 
